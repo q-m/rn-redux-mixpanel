b2bb6220c825ea13c2122b5681520d25886a1600
Tim Bormans <hello@timbormans.com>
2015-12-07 17:48:08 +0100

    Use fetch instead of superagent

--- src/api/sendMixpanelRequest.js
+++ src/api/sendMixpanelRequest.js
@@ -1,30 +1,30 @@
-import { Buffer } from 'buffer'
-import request from 'superagent'
+import {Buffer} from 'buffer';
 
 // Configuration Constants
-const DEBUG = typeof process === 'object' && process.env && process.env['NODE_ENV'] === 'development'
+const DEBUG = typeof process === 'object' && process.env && process.env['NODE_ENV'] === 'development';
 
 // Mixpanel Service Constants
-const MIXPANEL_REQUEST_PROTOCOL = 'https'
-const MIXPANEL_HOST = 'api.mixpanel.com'
+const MIXPANEL_REQUEST_PROTOCOL = 'https';
+const MIXPANEL_HOST = 'api.mixpanel.com';
 
-export default sendMixpanelRequest = ({ endpoint, data }) => {
-  const requestDataString = JSON.stringify(data)
-  const requestDataBase64String = new Buffer(requestDataString).toString('base64')
+export default sendMixpanelRequest = ({endpoint, data}) => {
+  const requestDataString = JSON.stringify(data);
+  const requestDataBase64String = new Buffer(requestDataString).toString('base64');
 
-  const requestUrl = `${MIXPANEL_REQUEST_PROTOCOL}://${MIXPANEL_HOST}${endpoint}?ip=1`
-  const req = request
-    .get(requestUrl)
-    .query(`data=${requestDataBase64String}`)
-    .end((error, res) => {
-      if (!DEBUG) {
-        return
-      }
+  const requestUrl = `${MIXPANEL_REQUEST_PROTOCOL}://${MIXPANEL_HOST}${endpoint}?ip=1&data=${requestDataBase64String}`;
 
-      if (error) {
-        console.log('mixpanel error:', error)
+  const req = fetch(requestUrl)
+    .then((response) => response.text())
+    .then((responseText) => {
+      if (DEBUG) {
+        console.log('mixpanel request:', requestUrl);
       }
     })
+    .catch((error) => {
+      if (DEBUG) {
+        console.warn('mixpanel error:', error);
+      }
+    });
 
-  return req
-}
+  return req;
+};
